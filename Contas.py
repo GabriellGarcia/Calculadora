0feb550e9d6c12796eeb802b37a3fa29d7a6d7a1
[EXT] - GABRIEL GARCIA MORAIS <gamorais@brasilseg.com.br>
2024-09-05 12:35:48 -0300

    Calcauladora

---
+++ Contas.py
@@ -0,0 +1,19 @@
+# Fazendo conta 
+Num3 = 26
+Num4 = 40
+result = Num3 * Num4
+print('O resultado da multniplicação é', result)
+
+# Pedir as pessoas que digite dois números
+num1 = float(input('Digite o primeiro número: '))
+num2 = float (input('Digite o segundo número: '))
+# Realizar os cálculos matemáticos
+Soma = num1 + num2
+subtração = num1 - num2
+multiplicação = num1 * num2
+divisão = num1 / num2 
+# imprimindo resultados 
+print(f'Soma: {Soma}')
+print(f'Subtração:{subtração}')
+print(f'Multiplicação: {multiplicação}')
+print(f'Divisão: {divisão}')
